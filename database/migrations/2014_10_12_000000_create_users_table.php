fe78bc90a167868bd0204dc2d44495216f52c978
coduori <coduori@finsense.co.ke>
2021-09-18 10:40:21 +0300

    Heroku procfile update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,6 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('msisdn')->unique()->nullable();
-            $table->integer('national_id')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
